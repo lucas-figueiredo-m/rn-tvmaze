296909309212b6e288110194dbddfe9896caf859
Lucas Figueiredo <lucas.figueiredo@engenharia.ufjf.br>
2021-11-23 00:39:43 -0300

    feature: adds icons to the tab bars

--- src/routes/navigators/TabNavigator.tsx
+++ src/routes/navigators/TabNavigator.tsx
@@ -4,6 +4,9 @@ import { Colors } from 'theme';
 import { TabRoutes } from 'routes/config';
 import ShowsList from 'screens/ShowsList/ShowsList';
 import SearchScreen from 'screens/SearchScreen/SearchScreen';
+import Home from 'assets/icons/home.svg'
+import Magnifyer from 'assets/icons/search.svg'
+import { SVG } from 'components/common';
 
 const Tabs = createBottomTabNavigator()
 
@@ -13,16 +16,24 @@ const TabNavigator: React.FC = () => (
       headerShown: false,
       // tabBarStyle: styles.tabBar,
       // tabBarLabelStyle: styles.label,
-      tabBarActiveTintColor: Colors.White
+      tabBarActiveTintColor: Colors.RoyalBlueDark
     }}
   >
     <Tabs.Screen
       name={TabRoutes.SHOWS_LIST}
       component={ShowsList}
+      options={{
+        tabBarLabel: 'Home',
+        tabBarIcon: ({ color }) => <SVG xml={Home} color={color} />
+      }}
     />
     <Tabs.Screen
       name={TabRoutes.SEARCH_SCREEN}
       component={SearchScreen}
+      options={{
+        tabBarLabel: 'Search',
+        tabBarIcon: ({ color }) => <SVG xml={Magnifyer} color={color} />
+      }}
     />
   </Tabs.Navigator>
 )
